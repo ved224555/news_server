84bccd8ac8c6b8de4174dafd544a835153f540e0
Ved <v4ved01@gmail.com>
2021-07-18 21:31:56 +0530

    zxbcnzdck

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 var cookieParser = require('cookie-parser');
 
-app.use(cors({origin:"*",credentials:true}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
